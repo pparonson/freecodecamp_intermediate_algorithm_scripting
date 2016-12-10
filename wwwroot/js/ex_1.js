a28ceceb38884cfc45b5af2564e1c48a8b603af1
pparonson <preston.aronson@gmail.com>
2016-12-10 09:48:36 -0700

    refactor ex_1 using module

--- wwwroot/js/ex_1.js
+++ wwwroot/js/ex_1.js
@@ -13,29 +13,29 @@
  //     let maxNum = null;
  //     let minNum = null;
  //     let sum = 0;
- //     for (var i = 0; i < arr.length - 1; i++) {
+ //     for (let i = 0; i < arr.length - 1; i++) {
  //         // Get min number of array
  //         minNum = Math.min( arr[i], arr[i + 1]);
  //         maxNum = Math.max( arr[i], arr[i + 1]);
  //     }
- //     for (var j = ( minNum + 1 ); j < maxNum; j++) {
+ //     for (let j = ( minNum + 1 ); j < maxNum; j++) {
  //         // increment from min to max
  //         arr.push( j );
  //
  //     }
- //     for (var k = 0; k < arr.length; k++) {
+ //     for ( let k = 0; k < arr.length; k++ ) {
  //         // return sum of all numbers in new arr
  //         sum += arr[k];
  //
  //     }
  //     return sum;
  // }
- // sumAll([1, 4]);
+ // sumAll( [ 1, 4 ] );
 
 // /*
 //  * Functional answer
 //  */
-const sumAll = (arr) => {
+const sumAll = ( arr ) => {
     // return sum of all numbers
     // Uses Math.min / Math.max to get range from array
     const getMinMax = mathFn => arr => mathFn( ...arr );
@@ -46,19 +46,22 @@ const sumAll = (arr) => {
     // increment from min to max
     // Recursion to sum all nums in range of min to max
     // Returns the value of acc ( sum of num in range)
-    const sumRange = (min, max, acc) => {
+    const sumRange = ( min, max, acc ) => {
         if (min > max) {
             return acc;
         }
-        return sumRange(min + 1, max, acc + min);
+        return sumRange( min + 1, max, acc + min );
     };
     // return the result
     // return sumRange(min(arr), max(arr), 0); // comment out if using module
 
-    Expose the function through a module
-    const _sumRange = sumRange(min(arr), max(arr), 0);
+    // Expose the function through a module
+    const _sumRange = sumRange( min( arr ), max( arr ), 0 );
     const module = {
         sumRange: _sumRange
     };
     return module;
 };
+
+// example
+const result = sumAll( [ 1, 4 ] );
